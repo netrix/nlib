c15173b859e1a60902755a8dae1b3364d54cc8a0
Kamil Szatkowski <kamil.szatkowski@gmail.com>
2012-06-01 00:46:43 +0200

    Memory tests passed

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -6,8 +6,5 @@ int main(int argc, char** argv)
 {
 	testing::InitGoogleTest(&argc, argv);
 	NAssert_StdAssert(true);
-
-	bool val = [&argc](int a, int b){ return a < b; }(1, 2);
-
 	return RUN_ALL_TESTS();
 }
